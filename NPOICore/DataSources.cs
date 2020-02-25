a3198b0cf9158b95c840a0889dca4e37fb098b08
ed2Fabdalpoz <f.dalpozzo@crif.com>
2020-02-25 14:43:08 +0100

    1st version

---
+++ NPOICore/DataSources.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NPOICore
+{
+    public class DataSource
+    {
+        public string Code { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public static class DataSources
+    {
+        public static List<DataSource> AreaManagers
+        {
+            get
+            {
+                return Enumerable.Range(0, 10).Select(x => new DataSource
+                {
+                    Code = "AMCode " + x,
+                    LastName = "AMLast Name " + x,
+                    FirstName = "AMName " + x
+                }).ToList();
+            }
+        }
+
+        public static List<DataSource> Sales
+        {
+            get
+            {
+                return Enumerable.Range(0, 10).Select(x => new DataSource
+                {
+                    Code = "SLCode " + x,
+                    LastName = "SLLast Name " + x,
+                    FirstName = "SLName " + x
+                }).ToList();
+            }
+        }
+
+        public static List<DataSource> Products
+        {
+            get
+            {
+                return Enumerable.Range(0, 10).Select(x => new DataSource
+                {
+                    Code = "PRCode " + x,
+                    LastName = "PRLast Name " + x,
+                    FirstName = "PRName " + x
+                }).ToList();
+            }
+        }
+    }
+}
---
+++ NPOICore/Program.cs
@@ -0,0 +1,106 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using NPOI.XSSF.UserModel;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NPOICore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var outputFileName = "PortfolioImportTemplate.xlsx";
+            using (var templateStream = new FileStream(outputFileName, FileMode.Open, FileAccess.Read))
+            {
+                var excel = new XSSFWorkbook(templateStream);
+                var sheet = (XSSFSheet)excel.GetSheetAt(0);
+
+                Task.Run(() =>
+                {
+                    CreateMetaDataRows(excel.CreateSheet("Area Manager"), DataSources.AreaManagers);
+                });
+
+                Task.Run(() =>
+                {
+                    CreateMetaDataRows(excel.CreateSheet("Sales"), DataSources.Sales);
+                });
+
+                Task.Run(() =>
+                {
+                    CreateMetaDataRows(excel.CreateSheet("Products"), DataSources.Products);
+                });
+                SetCodeColumn(sheet, "F", 6, 50000, "Area Manager");
+                SetCodeColumn(sheet, "G", 7, 50000, "Sales");
+                SetCodeColumn(sheet, "H", 8, 50000, "Products");
+
+                using (var outputStream = new FileStream(outputFileName, FileMode.Create, FileAccess.Write))
+                {
+                    excel.Write(outputStream);
+                }
+            }
+        }
+
+        private static void SetCodeColumn(XSSFSheet sheet, string columnLetter, int colindex, int elements, string sheetName)
+        {
+            var row0 = sheet.GetRow(0);
+            var lastColIndex = row0.LastCellNum;
+            var headerCell = row0.CreateCell(lastColIndex, CellType.String);
+            headerCell.SetCellValue(sheetName + " Code");
+            row0.Cells.Add(headerCell);
+            headerCell.CellStyle = sheet.Workbook?.GetSheetAt(0)?.GetRow(0)?.GetCell(0)?.CellStyle;
+
+            for (var i = 1; i <= elements; i++)
+            {
+                var row = sheet.GetRow(i) ?? sheet.CreateRow(i);
+                var cell = row.CreateCell(lastColIndex, CellType.Formula);
+                var formula = $"INDEX('{sheetName}'!A1:D{elements},MATCH(${columnLetter}{i + 1},'{sheetName}'!D1:D{elements},0),1)";
+                cell.SetCellFormula(formula);
+            }
+            sheet.SetColumnHidden(row0.LastCellNum - 1, true);
+
+            var validationHelper = new XSSFDataValidationHelper(sheet);
+            var addressList = new CellRangeAddressList(0, elements - 1, colindex - 1, colindex - 1);
+            var constraint = validationHelper.CreateFormulaListConstraint($"'{sheetName}'!$D$2:$D$" + elements);
+            var dataValidation = validationHelper.CreateValidation(constraint, addressList);
+            sheet.AddValidationData(dataValidation);
+        }
+
+        private static void CreateMetaDataRows(ISheet sheet, List<DataSource> items)
+        {
+            CreateRow(sheet, "Code", "First Name", "Last Name", "Full Name", 0);
+            for (var i = 1; i < items.Count; i++)
+                CreateRow(sheet, items[i].Code, items[i].FirstName, items[i].LastName, items[i].LastName + " " + items[i].FirstName, i);
+            sheet.SetColumnHidden(3, true);
+            Enumerable.Range(0, 4).ToList().ForEach(i => sheet.AutoSizeColumn(i));
+        }
+
+        private static void CreateRow(ISheet sheet, string code, string firstname, string lastname, string fullname, int i)
+        {
+            IRow row = sheet.CreateRow(i);
+
+            var codeCell = row.CreateCell(0);
+            codeCell.SetCellValue(code);
+
+            var fnCell = row.CreateCell(1);
+            fnCell.SetCellValue(firstname);
+
+            var lnCell = row.CreateCell(2);
+            lnCell.SetCellValue(lastname);
+
+            var funCell = row.CreateCell(3);
+            funCell.SetCellValue(fullname);
+
+            if (i == 0)
+            {
+                var styleCell = sheet.Workbook.GetSheetAt(0).GetRow(0).GetCell(0);
+                codeCell.CellStyle = styleCell.CellStyle;
+                fnCell.CellStyle = styleCell.CellStyle;
+                lnCell.CellStyle = styleCell.CellStyle;
+                funCell.CellStyle = styleCell.CellStyle;
+            }
+        }
+    }
+}

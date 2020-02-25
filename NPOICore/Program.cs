7ff6f04f124f6592ed43af49754732f614cca2be
ed2Fabdalpoz <f.dalpozzo@crif.com>
2020-02-25 15:15:30 +0100

    Paramaterized version

--- NPOICore/Program.cs
+++ NPOICore/Program.cs
@@ -14,7 +14,7 @@ namespace NPOICore
             var outputFileName = "PortfolioImportTemplate.xlsx";
             using (var templateStream = new FileStream(outputFileName, FileMode.Open, FileAccess.Read))
             {
-                XSSFWorkbook excel = CompleteVersionWithCodes(templateStream);
+                XSSFWorkbook excel = UpdateExcel(templateStream, 5000, true);
 
                 using (var outputStream = new FileStream(outputFileName, FileMode.Create, FileAccess.Write))
                 {
@@ -23,37 +23,40 @@ namespace NPOICore
             }
         }
 
-        private static XSSFWorkbook CompleteVersionWithCodes(FileStream templateStream)
+        private static XSSFWorkbook UpdateExcel(FileStream templateStream, int length, bool createCodeColumns)
         {
             var excel = new XSSFWorkbook(templateStream);
             var sheet = (XSSFSheet)excel.GetSheetAt(0);
 
-            CreateMetaDataRows(excel.CreateSheet("Area Manager"), DataSources.AreaManagers);
-            SetCodeColumn(sheet, "F", 6, 50000, "Area Manager");
-            CreateMetaDataRows(excel.CreateSheet("Sales"), DataSources.Sales);
-            SetCodeColumn(sheet, "G", 7, 50000, "Sales");
-            CreateMetaDataRows(excel.CreateSheet("Products"), DataSources.Products);
-            SetCodeColumn(sheet, "H", 8, 50000, "Products");
+            CreateMetaDataRows(excel.CreateSheet("Area Manager"), DataSources.AreaManagers, createCodeColumns);
+            SetCodeColumn(sheet, "F", 6, length, "Area Manager", createCodeColumns);
+            CreateMetaDataRows(excel.CreateSheet("Sales"), DataSources.Sales, createCodeColumns);
+            SetCodeColumn(sheet, "G", 7, length, "Sales", createCodeColumns);
+            CreateMetaDataRows(excel.CreateSheet("Products"), DataSources.Products, createCodeColumns);
+            SetCodeColumn(sheet, "H", 8, length, "Products", createCodeColumns);
             return excel;
         }
 
-        private static void SetCodeColumn(XSSFSheet sheet, string columnLetter, int colindex, int elements, string sheetName)
+        private static void SetCodeColumn(XSSFSheet sheet, string columnLetter, int colindex, int elements, string sheetName, bool createCodeColumns)
         {
-            var row0 = sheet.GetRow(0);
-            var lastColIndex = row0.LastCellNum;
-            var headerCell = row0.CreateCell(lastColIndex, CellType.String);
-            headerCell.SetCellValue(sheetName + " Code");
-            row0.Cells.Add(headerCell);
-            headerCell.CellStyle = sheet.Workbook?.GetSheetAt(0)?.GetRow(0)?.GetCell(0)?.CellStyle;
-
-            for (var i = 1; i <= elements; i++)
+            if (createCodeColumns)
             {
-                var row = sheet.GetRow(i) ?? sheet.CreateRow(i);
-                var cell = row.CreateCell(lastColIndex, CellType.Formula);
-                var formula = $"INDEX('{sheetName}'!A1:D{elements},MATCH(${columnLetter}{i + 1},'{sheetName}'!D1:D{elements},0),1)";
-                cell.SetCellFormula(formula);
+                var row0 = sheet.GetRow(0);
+                var lastColIndex = row0.LastCellNum;
+                var headerCell = row0.CreateCell(lastColIndex, CellType.String);
+                headerCell.SetCellValue(sheetName + " Code");
+                row0.Cells.Add(headerCell);
+                headerCell.CellStyle = sheet.Workbook?.GetSheetAt(0)?.GetRow(0)?.GetCell(0)?.CellStyle;
+
+                for (var i = 1; i <= elements; i++)
+                {
+                    var row = sheet.GetRow(i) ?? sheet.CreateRow(i);
+                    var cell = row.CreateCell(lastColIndex, CellType.Formula);
+                    var formula = $"INDEX('{sheetName}'!A1:D{elements},MATCH(${columnLetter}{i + 1},'{sheetName}'!D1:D{elements},0),1)";
+                    cell.SetCellFormula(formula);
+                }
+                sheet.SetColumnHidden(row0.LastCellNum - 1, true);
             }
-            sheet.SetColumnHidden(row0.LastCellNum - 1, true);
 
             var validationHelper = new XSSFDataValidationHelper(sheet);
             var addressList = new CellRangeAddressList(0, elements - 1, colindex - 1, colindex - 1);
@@ -62,11 +65,14 @@ namespace NPOICore
             sheet.AddValidationData(dataValidation);
         }
 
-        private static void CreateMetaDataRows(ISheet sheet, List<DataSource> items)
+        private static void CreateMetaDataRows(ISheet sheet, List<DataSource> items, bool createCodeColumns)
         {
             CreateRow(sheet, "Code", "First Name", "Last Name", "Full Name", 0);
             for (var i = 1; i < items.Count; i++)
-                CreateRow(sheet, items[i].Code, items[i].FirstName, items[i].LastName, items[i].LastName + " " + items[i].FirstName, i);
+            {
+                var fullName = createCodeColumns ? $"{items[i].LastName} {items[i].FirstName}" : $"{items[i].LastName} {items[i].FirstName} ({items[i].Code})";
+                CreateRow(sheet, items[i].Code, items[i].FirstName, items[i].LastName, fullName, i);
+            }
             sheet.SetColumnHidden(3, true);
             Enumerable.Range(0, 4).ToList().ForEach(i => sheet.AutoSizeColumn(i));
         }
